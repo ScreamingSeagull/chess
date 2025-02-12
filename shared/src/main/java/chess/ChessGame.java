d0a5f555644f7d8c646f74a69076fab765676c35
ScreamingSeagull <corbiniclark@gmail.com>
2025-02-11 23:13:06 -0700

    I am going to kill myself

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -32,6 +32,7 @@ public class ChessGame {
             return null;
         }
         Collection<ChessMove> moves = cboard.getPiece(startPosition).pieceMoves(cboard, startPosition); //Issue about being static because it was not assigned to a piece, or the cboard.getPiece(startPosition) was not included
+        Collection<ChessMove> answers = cboard.getPiece(startPosition).pieceMoves(cboard, startPosition); //Setting answers equal to moves results in them just pointing to each other? Breaks modification of collection while iterating through it below
         ChessBoard tempboard = new ChessBoard();
         ChessPosition temp = new ChessPosition(1, 1);
         for (ChessMove current : moves){
@@ -42,35 +43,31 @@ public class ChessGame {
                     tempboard.addPiece(temp, cboard.getPiece(temp));
                 }
             }
-            tempboard = cboard;
             tempboard.addPiece(current.getEndPosition(), tempboard.getPiece(current.getStartPosition())); //Sets old piece to new location
             tempboard.addPiece(current.getStartPosition(), null); //Wipes old location as null
-            if (isInCheck(currentColor, tempboard)){
-                moves.remove(current);
+            if (isInCheck(currentColor, tempboard) || isInCheckmate(currentColor, tempboard)){
+                answers.remove(current);
             }
         }
-        return moves;
+        return answers;
         //Take input position on the chessboard, return all moves legal to make
         //Only valid if it's a piece move for the piece at the input location
         //Only valid if it does not place the King into check
     }
 
     public void makeMove(ChessMove move) throws InvalidMoveException {
-        if (cboard.getPiece(move.getStartPosition()).getTeamColor() == currentColor){ //Checks who's team it is, need a way to update this unless the tests already do? Not sure, failing earlier, maybe invert?
+        if (cboard.getPiece(move.getStartPosition()) == null){
+            //No item found
+        }
+        else if (cboard.getPiece(move.getStartPosition()).getTeamColor() == currentColor){
             Collection<ChessMove> answers = validMoves(move.getStartPosition());
             if (answers.contains(move)) {
                 cboard.addPiece(move.getEndPosition(), cboard.getPiece(move.getStartPosition())); //Sets old piece to new location
                 cboard.addPiece(move.getStartPosition(), null); //Wipes old location as null
-            }
-            else{
-                throw new InvalidMoveException();
+                if (currentColor == TeamColor.WHITE) currentColor = TeamColor.BLACK;
+                else currentColor = TeamColor.WHITE;
             }
         }
-        else{
-            throw new InvalidMoveException();
-        }
-        if (currentColor == TeamColor.WHITE) currentColor = TeamColor.BLACK;
-        else currentColor = TeamColor.WHITE;
     }
 
     public ChessPosition findKing(TeamColor color, ChessBoard board) { //Finds king of set color
@@ -98,7 +95,7 @@ public class ChessGame {
                 if(cboard.getPiece(current) != null) {
                     Collection<ChessMove> possibleattack = cboard.getPiece(current).pieceMoves(cboard, current); //Collects possible attacks for pieces
                     ChessMove attack = new ChessMove(current, kingpos, null); //Temporary move where the attack lands on the King *****************WARNING: MAY HAVE ISSUES IF PROMOTING AFTER AN ATTACK AS PIECE TYPE IS NULL, ENSURE ATTACK IS ABLE TO RECEIVE PROMOTIONS IN SEPARATE AREA
-                    if (possibleattack.contains(attack) && (kingmoves != null)) { //If this attack is true and the King can still move out of the way
+                    if (possibleattack.contains(attack) && !kingmoves.isEmpty()) { //If this attack is true and the King can still move out of the way
                         return true;
                     }
                 }
@@ -107,7 +104,7 @@ public class ChessGame {
         return false;
     }
     public boolean isInCheck(TeamColor teamColor, ChessBoard board) { //Checks if in check in a separate board
-        ChessPosition kingpos = findKing(teamColor, cboard); //Finds king position
+        ChessPosition kingpos = findKing(teamColor, board); //Finds king position
         Collection<ChessMove> kingmoves = board.getPiece(kingpos).pieceMoves(board, kingpos); //Finds valid moves for king, null if none
         ChessPosition current = new ChessPosition(1, 1); //Starts at 1, 1
         for (int i = 1; i <=8; i++){
@@ -117,7 +114,7 @@ public class ChessGame {
                 if(board.getPiece(current) != null) {
                     Collection<ChessMove> possibleattack = board.getPiece(current).pieceMoves(board, current); //Collects possible attacks for pieces
                     ChessMove attack = new ChessMove(current, kingpos, null); //Temporary move where the attack lands on the King
-                    if (possibleattack.contains(attack) && (kingmoves != null)) { //If this attack is true and the King can still move out of the way
+                    if (possibleattack.contains(attack) && !kingmoves.isEmpty()) { //If this attack is true and the King can still move out of the way
                         return true;
                     }
                 }
@@ -129,6 +126,12 @@ public class ChessGame {
     public boolean isInCheckmate(TeamColor teamColor) {
         ChessPosition kingpos = findKing(teamColor, cboard); //Finds king position
         Collection<ChessMove> kingmoves = cboard.getPiece(kingpos).pieceMoves(cboard, kingpos);
+        Collection<ChessMove> actualmoves = cboard.getPiece(kingpos).pieceMoves(cboard, kingpos);
+        for (ChessMove possible : kingmoves) {
+            if (!kingMove(currentColor, possible, cboard)) {
+                actualmoves.remove(possible);
+            }
+        }
         ChessPosition current = new ChessPosition(1, 1);
         for (int i = 1; i <=8; i++){
             current.changeRow(i);
@@ -137,7 +140,7 @@ public class ChessGame {
                 if(cboard.getPiece(current) != null) {
                     Collection<ChessMove> possibleattack = cboard.getPiece(current).pieceMoves(cboard, current);
                     ChessMove attack = new ChessMove(current, kingpos, null);
-                    if (possibleattack.contains(attack) && (kingmoves == null)) {
+                    if (possibleattack.contains(attack) && kingmoves.isEmpty()) {
                         return true;
                     }
                 }
@@ -147,8 +150,14 @@ public class ChessGame {
     }
 
     public boolean isInCheckmate(TeamColor teamColor, ChessBoard board) {
-        ChessPosition kingpos = findKing(teamColor, cboard); //Finds king position
-        Collection<ChessMove> kingmoves = board.getPiece(kingpos).pieceMoves(board, kingpos);
+        ChessPosition kingpos = findKing(teamColor, board); //Finds king position
+        Collection<ChessMove> kingmoves = cboard.getPiece(kingpos).pieceMoves(cboard, kingpos);
+        Collection<ChessMove> actualmoves = cboard.getPiece(kingpos).pieceMoves(cboard, kingpos);
+        for (ChessMove possible : kingmoves) {
+            if (!kingMove(currentColor, possible, cboard)) {
+                actualmoves.remove(possible);
+            }
+        }
         ChessPosition current = new ChessPosition(1, 1);
         for (int i = 1; i <=8; i++){
             current.changeRow(i);
@@ -157,13 +166,34 @@ public class ChessGame {
                 if(board.getPiece(current) != null) {
                     Collection<ChessMove> possibleattack = board.getPiece(current).pieceMoves(board, current);
                     ChessMove attack = new ChessMove(current, kingpos, null);
-                    if (possibleattack.contains(attack) && (kingmoves == null)) {
+                    if (possibleattack.contains(attack) && kingmoves.isEmpty()) {
                         return true;
                     }
                 }
             }
         }
         return false;
+    }
+
+    public boolean kingMove(TeamColor color, ChessMove move, ChessBoard board) { //RETURNS FALSE IF NOT ABLE TO MOVE INTO SET SPACE
+        ChessBoard tempboard = board;
+        tempboard.addPiece(move.getEndPosition(), tempboard.getPiece(move.getStartPosition()));
+        tempboard.addPiece(move.getStartPosition(), null);
+        ChessPosition current = new ChessPosition(1, 1);
+        for (int i = 1; i <=8; i++){
+            current.changeRow(i);
+            for (int j = 1; j <=8; j++){
+                current.changeColumn(j);
+                if(tempboard.getPiece(current) != null && tempboard.getPiece(current).getTeamColor() != color) {
+                    Collection<ChessMove> possibleattack = tempboard.getPiece(current).pieceMoves(tempboard, current);
+                    ChessMove attack = new ChessMove(current, move.getEndPosition(), null);
+                    if (possibleattack.contains(attack)) { //If the King enters a check/checkmate position
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     public boolean isInStalemate(TeamColor teamColor) {
@@ -178,7 +208,7 @@ public class ChessGame {
                     Collection<ChessMove> possibleattack = cboard.getPiece(current).pieceMoves(cboard, current);
                     for (int k = 0; k <= possibleattack.size(); k++) {
                         ChessMove attack = new ChessMove(current, kingpos, cboard.getPiece(current).getPieceType());
-                        if (!(possibleattack.contains(attack)) && (kingmoves == null)) {
+                        if (!(possibleattack.contains(attack))) {//ADD KINGMOVES CHECK************************************************************************************************************
                             return true;
                         }
                     }

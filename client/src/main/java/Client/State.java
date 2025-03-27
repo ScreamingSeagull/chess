bedd87e4d74daf510bcd081786bb8e180c2058f7
ScreamingSeagull <corbiniclark@gmail.com>
2025-03-27 11:57:15 -0600

    Additional work

--- client/src/main/java/Client/State.java
+++ client/src/main/java/Client/State.java
@@ -4,5 +4,5 @@ public enum State {
     SIGNEDOUT,
     SIGNEDIN,
     PLAYING,
-    OBSERVING
+    WATCHING
 }
--- client/src/main/java/Client/client.java
+++ client/src/main/java/Client/client.java
@@ -1,17 +1,19 @@
 package Client;
 
 import exception.ResponseException;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
+//import client.websocket.WebSocketFacade;
 import java.util.Arrays;
 
 public class client {
-    private final ServerFacade serverFacade;
+    private final ServerFacade server;
+    //private WebSocketFacade ws;
+    private State state = State.SIGNEDOUT;
     public client(String domainName) throws URISyntaxException, IOException {
-        serverFacade = new ServerFacade(domainName);
+        server = new ServerFacade(domainName);
     }
-    public void eval(String input) {
+    public void eval(String input) { //This needs to return data of differing kinds, condense into json?
         String[] line = input.toLowerCase().split(" ");
         String cmd;
         if (line.length == 0) {
@@ -40,21 +42,29 @@ public class client {
 
     }
     public void register(String... params) {
-        if (params.length == 3){
-
+        if (params.length == 3){ //Username, password, email
+            //ws.register();
+            //Return register result
         }
         throw new ResponseException(params.length, "Incorrect number of parameters provided");
     }
     public void login(String... params) {
-
+        if (params.length == 2) { //Username, password
+            state = State.SIGNEDIN;
+            //ws = new WebSocketFacade(server);
+            //ws.login(username, password);
+            //Return login result
+        }
     }
     public void logout(){
 
     }
     public void list(){
+        //Return listgames result
 
     }
     public void create(String... params){
+        //Return creategames result
 
     }
     public void join(String... params){

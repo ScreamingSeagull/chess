836ef1ab71fe42b20156d7a4f7597ddb82b74dba
ScreamingSeagull <corbiniclark@gmail.com>
2025-03-28 19:28:48 -0600

    Working grid

--- client/src/main/java/Client/client.java
+++ client/src/main/java/Client/client.java
@@ -6,17 +6,25 @@ import model.request.CreateGameRequest;
 import model.request.JoinGameRequest;
 import model.request.LoginRequest;
 import model.request.RegisterRequest;
+import java.nio.charset.StandardCharsets;
+import java.awt.*;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Collection;
+
+import static java.sql.Types.NULL;
 import static ui.EscapeSequences.*;
 
 public class client {
     private final ServerFacade server;
     private State state = State.SIGNEDOUT;
+    PrintStream out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+    ChessBoard board = new ChessBoard();
     public client(String domainName) throws URISyntaxException, IOException {
         server = new ServerFacade(domainName);
+        board.resetBoard();
     }
     public void eval(String input) { //This needs to return data of differing kinds, condense into json?
         String[] line = input.toLowerCase().split(" ");
@@ -44,7 +52,8 @@ public class client {
         }
     }
     public void clearDB() {
-        server.clearDB();
+        printG(out);
+        //server.clearDB();
     }
     public void register(String... params) {
         RegisterRequest req = new RegisterRequest(params[0], params[1], params[2]); //Check parameters
@@ -72,69 +81,110 @@ public class client {
     public void help(){
         //Not handled by server, keep local
     }
-    private void prompt() {
-        System.out.println("\n" + RESET_TEXT_COLOR + ">>>" + SET_TEXT_COLOR_GREEN);
-    }
-    private void printGame() {
-        stringBoard(null, null, false);
-        prompt();
-    }
-    private String stringBoard(ChessBoard board, Collection<ChessPosition> locations, boolean black){
-        int up = black ? 1:8;
-        int down = black ? 8:1;
-        int diff = black ? -1:1;
-        StringBuilder sb = new StringBuilder();
-        boolean alt = true;
-        buildUp(sb, black);
-        boolean altTile = true; //switch between the two each time, boolean = !boolean
-        buildTile(sb, altTile, ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN, true);
-        sb.append(RESET_BG_COLOR + '\n');
-        return "null";
-    }
-    private void buildUp(StringBuilder sb, boolean black) {
-        int up = black ? 1:8;
-        int down = black ? 8:1;
-        char diff = (char) (black ? -1:1);
-        buildBorder(sb);
-    }
-    private void buildBorder(StringBuilder sb) {
-        sb.append(SET_BG_COLOR_YELLOW + SET_TEXT_COLOR_WHITE);
-        sb.append("dimensions");
-        sb.append("*");
-        sb.append("dimensions");
-    }
-    private void buildTile(StringBuilder sb, boolean altColor, ChessGame.TeamColor teamColor, ChessPiece.PieceType piece, boolean selected) {
-        if (selected) {
-            sb.append("selected");
-        } else if (altColor) {
-            sb.append("altcolor");
-        } else {
-            sb.append("basic color");
+    private void printHorizontalBorder(PrintStream out) {
+        drawSquare(out, null, null, null);
+        for (int i = 0; i<8; i++) {
+            drawSquare(out, null, null, (char)('A' + i));
         }
-        if (teamColor == ChessGame.TeamColor.WHITE) {
-            sb.append(SET_TEXT_COLOR_WHITE);
-        } else {
-            sb.append(SET_TEXT_COLOR_BLACK);
+        drawSquare(out, null, null, null);
+        out.println(RESET_BG_COLOR);
+    }
+    private char convert(ChessPiece piece) {
+        if (piece == null || piece.getPieceType() == null) {
+            return ' ';
+        }
+        switch(piece.getPieceType()) {
+            case KING:
+                return 'K';
+            case QUEEN:
+                return 'q';
+            case ROOK:
+                return 'r';
+            case BISHOP:
+                return 'b';
+            case KNIGHT:
+                return 'k';
+            case PAWN:
+                return 'p';
+            default:
+                return ' ';
         }
     }
-    private void buildPiece(StringBuilder sb, ChessGame.TeamColor teamColor, ChessPiece.PieceType piece) { //May need config options for printing?
-        if (piece != null) {
-            switch(piece) {
-                case KING:
-                    break;
-                case QUEEN:
-                    break;
-                case BISHOP:
-                    break;
-                case ROOK:
-                    break;
-                case KNIGHT:
-                    break;
-                case PAWN:
-                    break;
+
+    private void printG(PrintStream out) {
+        printHorizontalBorder(out);
+        for (int boardRow = 0; boardRow < 8; ++boardRow) {
+            drawSquare(out, null, null, (char)(boardRow + 49)); //Using ascii interpretation, int converted to char of set number
+            for (int boardCol = 0; boardCol < 8; ++boardCol) {
+                if ((boardRow + boardCol) % 2 == 0) {
+                    drawSquare(out, Color.white, Color.BLACK, convert(board.getPiece(boardRow + 1, boardCol + 1)));
+                } else {
+                    drawSquare(out, Color.black, Color.WHITE, ' ');
+                }
             }
-        } else {
-            sb.append("n/a");
+            drawSquare(out, null, null, (char)(boardRow + 49));
+            out.println(RESET_BG_COLOR);
         }
+        printHorizontalBorder(out);
+    }
+    private void drawSquare(PrintStream out, Color squareColor, Color pieceColor, Character c) {
+        if (squareColor == Color.WHITE){
+            out.print(SET_BG_COLOR_LIGHT_GREY);
+        } else if (squareColor == Color.BLACK){
+            out.print(SET_BG_COLOR_DARK_GREY);
+        } else if (squareColor == null) {
+            out.print(SET_BG_COLOR_WHITE);
+        }
+        if (pieceColor == Color.WHITE){
+            out.print(SET_TEXT_COLOR_WHITE);
+        } else if (pieceColor == Color.BLACK){
+            out.print(SET_TEXT_COLOR_BLACK);
+        } else if (pieceColor == null) {
+            out.print(SET_TEXT_COLOR_LIGHT_GREY);
+        }
+        if (c == null) {
+            out.print("\u2005\u2005 \u2005\u2005");
+        } else {
+            out.print("\u2005\u2005" + c + "\u2005\u2005");
+            }
     }
 }
+
+//    private String stringBoard(ChessBoard board, Collection<ChessPosition> locations, boolean black){
+//        int up = black ? 1:8;
+//        int down = black ? 8:1;
+//        int diff = black ? -1:1;
+//        StringBuilder sb = new StringBuilder();
+//        boolean alt = true;
+//        buildUp(sb, black);
+//        boolean altTile = true; //switch between the two each time, boolean = !boolean
+//        buildTile(sb, altTile, ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN, true);
+//        sb.append(RESET_BG_COLOR + '\n');
+//        return "null";
+//    }
+//    private void buildUp(StringBuilder sb, boolean black) {
+//        int up = black ? 1:8;
+//        int down = black ? 8:1;
+//        char diff = (char) (black ? -1:1);
+//        buildBorder(sb);
+//    }
+//    private void buildBorder(StringBuilder sb) {
+//        sb.append(SET_BG_COLOR_YELLOW + SET_TEXT_COLOR_WHITE);
+//        sb.append("dimensions");
+//        sb.append("*");
+//        sb.append("dimensions");
+//    }
+//    private void buildTile(StringBuilder sb, boolean altColor, ChessGame.TeamColor teamColor, ChessPiece.PieceType piece, boolean selected) {
+//        if (selected) {
+//            sb.append("selected");
+//        } else if (altColor) {
+//            sb.append("altcolor");
+//        } else {
+//            sb.append("basic color");
+//        }
+//        if (teamColor == ChessGame.TeamColor.WHITE) {
+//            sb.append(SET_TEXT_COLOR_WHITE);
+//        } else {
+//            sb.append(SET_TEXT_COLOR_BLACK);
+//        }
+//    }

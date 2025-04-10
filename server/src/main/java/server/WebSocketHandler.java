01d279c584a23dd6a9fff429acd7f2f100d6a1df
ScreamingSeagull <corbiniclark@gmail.com>
2025-04-10 15:31:49 -0600

    Working server initialization

--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -1,7 +1,14 @@
 package server;
 
 import dataaccess.*;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.eclipse.jetty.websocket.api.Session;
+import com.google.gson.Gson;
+import websocket.commands.UserGameCommand;
 
+
+@WebSocket
 public class WebSocketHandler {
     private final UserDAO userDAO;
     private final AuthDAO authDAO;
@@ -10,5 +17,8 @@ public class WebSocketHandler {
         this.userDAO = userDAO;
         this.authDAO = authDAO;
         this.gameDAO = gameDAO;
+    }
+    @OnWebSocketMessage public void onMessage(Session session, String s) throws DataAccessException {
+        UserGameCommand command = new Gson().fromJson(s, UserGameCommand.class);
     }
 }

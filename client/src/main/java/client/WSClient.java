46c2fea1ee03ad70bd5664e24bbf7e63ac4d3dc0
ScreamingSeagull <corbiniclark@gmail.com>
2025-04-09 21:49:21 -0600

    Additional pseudocode

--- client/src/main/java/client/WSClient.java
+++ client/src/main/java/client/WSClient.java
@@ -1,39 +1,44 @@
 package client;
 
-import com.sun.nio.sctp.NotificationHandler;
+import exception.ResponseException;
 import org.eclipse.jetty.websocket.api.Session;
-import spark.Spark;
-
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import javax.websocket.*;
-import java.io.IOException;
 import java.net.URI;
+import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Scanner;
 
 public class WSClient extends Endpoint {
-    private final String serverUrl;
-    private final NotificationHandler notifHandler;
-    private Session session;
-    public WSClient(String serverUrl, NotificationHandler notificationHandler) throws DeploymentException, IOException, URISyntaxException {
-        URI uri = new URI("ws://localhost:8080/ws");
-        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-        this.session = (Session) container.connectToServer(this, uri);
-        this.session.addMessageHandler(new MessageHandler.Whole<String>);
 
-        var ws = new WSClient();
-        this.serverUrl = serverUrl;
-        this.notifHandler = notificationHandler;
-        Spark.webSocket("/ws", Client.class);
+    public javax.websocket.Session session;
+//    public static void main(String[] args) {
+//        Spark.port(8080);
+//        Spark.webSocket("/ws", WSClient.class);
+//        Spark.get("/echo/:msg", (req, res) -> "HTTP response: " + req.params(":msg"));
+//    }
+    public WSClient(String serverDomain) throws DeploymentException, IOException, URISyntaxException {
+        try {
+            URI uri = new URI(String.format("ws://%s/connect", serverDomain));
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, uri);
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                public void onMessage(String message) {
+                    System.out.println(message);
+                }
+            });
+        } catch (DeploymentException | IOException| URISyntaxException e) {
+            throw new ResponseException(500, e.getMessage());
+        }
     }
-    public void onMessage(Session session, String message) throws IOException {
-        System.out.printf("received: %s", message);
-        session.getRemote().sendString("WebSocket response: " + message);
-    }
-    public void send(String msg) throws Exception {
+//    @OnWebSocketMessage
+//    public void onMessage(Session session, String message) throws IOException {
+//        System.out.printf("received: %s", message);
+//        session.getRemote().sendString("WebSocket response: " + message);
+//    }
+    public void send(String msg) throws IOException {
         this.session.getBasicRemote().sendText(msg);
     }
     @Override
     public void onOpen(javax.websocket.Session session, EndpointConfig endpointConfig) {
-
     }
 }
